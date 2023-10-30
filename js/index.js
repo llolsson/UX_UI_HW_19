9d08ac2e219e7831d42d255fbe5ff8faee8a0ef2
llolsson <132418076+llolsson@users.noreply.github.com>
2023-10-30 15:53:51 -0400

    loading animation 1

---
+++ js/index.js
@@ -0,0 +1,26 @@
+function fade() {
+  $('.preloader').fadeOut("slow");
+  }
+  setTimeout(fade, 3000);
+
+
+
+
+
+  $(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          window.location.hash = hash;
+        });
+      }
+    })
+  })
